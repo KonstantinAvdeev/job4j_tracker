eb40c331c5ae36cd5bc27059f4623b7246080d0d
Konstantin Avdeev <fujiun91@gmail.com>
2022-10-17 20:35:01 +0300

    2. Трекер SQL [#1734]

---
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -0,0 +1,148 @@
+package ru.job4j.tracker;
+
+import ru.job4j.tracker.model.Item;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class SqlTracker implements Store, AutoCloseable {
+
+    private Connection cn;
+
+    public SqlTracker() {
+        init();
+    }
+
+    public void init() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cn != null) {
+            cn.close();
+        }
+    }
+
+    @Override
+    public Item add(Item item) {
+        try (PreparedStatement statement =
+                     cn.prepareStatement("insert into items(id, name, created) values (?, ?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setInt(1, item.getId());
+            statement.setString(2, item.getName());
+            statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     cn.prepareStatement("update items set name = ?, created = ? where id = ?")) {
+            statement.setString(1, item.getName());
+            statement.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+            statement.setInt(3, item.getId());
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     cn.prepareStatement("delete from items where id = ?")) {
+            statement.setInt(1, id);
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement("select * from items;")) {
+            try (ResultSet set = statement.getResultSet()) {
+                while (set.next()) {
+                    items.add(new Item(set.getInt("id"), set.getString("name"),
+                            set.getTimestamp("created").toLocalDateTime()));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement("select * from items where name = ?;")) {
+            statement.setString(1, key);
+            try (ResultSet set = statement.getResultSet()) {
+                while (set.next()) {
+                    for(Item item:items){
+                        if(key.equals(item.getName())){
+                            items.add(new Item(set.getInt(1), set.getString(2),
+                                    set.getTimestamp(3).toLocalDateTime()));
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Item item = null;
+        try (PreparedStatement statement = cn.prepareStatement("select * from items where id = ?;")) {
+            statement.setInt(1, id);
+            try (ResultSet set = statement.getResultSet()) {
+                while (set.next() && id == set.getInt(1)) {
+                    item = new Item(set.getInt(1), set.getString(2),
+                            set.getTimestamp(3).toLocalDateTime());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+}
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class StartUI {
 
-    public void init(Input input, MemTracker tracker, List<UserAction> actions) {
+    public void init(Input input, Store tracker, List<UserAction> actions) {
         boolean run = true;
         while (run) {
             showMenu(actions);
@@ -31,20 +31,24 @@ public class StartUI {
 
 
     public static void main(String[] args) {
-        Input validate = new ValidateInput(
+        Input input = new ValidateInput(
                 new ConsoleInput()
         );
         Output output = new ConsoleOutput();
-        List<UserAction> actions = List.of(
-                new CreateAction(output),
-                new ReplaceAction(output),
-                new DeleteAction(output),
-                new FindAllAction(output),
-                new FindByIdAction(output),
-                new FindByNameAction(output),
-                new ExitAction()
-        );
-        MemTracker tracker = new MemTracker();
-        new StartUI().init(validate, tracker, actions);
+        try (SqlTracker tracker = new SqlTracker()) {
+            tracker.init();
+            List<UserAction> actions = List.of(
+                    new CreateAction(output),
+                    new ReplaceAction(output),
+                    new DeleteAction(output),
+                    new FindAllAction(output),
+                    new FindByIdAction(output),
+                    new FindByNameAction(output),
+                    new ExitAction()
+            );
+            new StartUI().init(input, tracker, actions);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
